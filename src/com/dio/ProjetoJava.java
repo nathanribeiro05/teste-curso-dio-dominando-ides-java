0e6d6178bd12e0b1060ad7ea584225c8029f269a
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-03-10 11:12:25 -0300

    second commit

--- src/com/dio/ProjetoJava.java
+++ src/com/dio/ProjetoJava.java
@@ -3,7 +3,9 @@ package com.dio;
 public class ProjetoJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 3;
+		int b = 2;
+		System.out.println("Hello World! " + (a+b));
 
 	}
 
